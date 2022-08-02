b1f1eddc4b9518bdb12fe521583dc53116634143
KalakCoder <gitkalak@gmail.com>
2022-08-02 21:32:51 +0545

    youtube

---
+++ assets/js/men.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "para-middle") {
+      x.className += " responsive";
+    } else {
+      x.className = "para-middle";
+    }
+  }
+  
+
